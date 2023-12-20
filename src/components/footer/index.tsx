ab1ee97f68d9bb7930f4bdbb0b756082010131cc
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-12-20 11:52:25 +0200

    correct a footer add form

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,8 +1,32 @@
+'use client';
+
 import css from './footer.module.scss';
+import { useState } from 'react';
+
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
 
 export const Footer = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event: {
+    currentTarget: any;
+    preventDefault: () => void;
+    stopPropagation: () => void;
+  }) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
   return (
-    <footer>
+    <footer className={css.footer}>
       <address>
         <ul className={css.address}>
           <li className={css.address__item}>
@@ -29,9 +53,34 @@ export const Footer = () => {
           </li>
         </ul>
       </address>
-      <div>
-        <p>form</p>
-      </div>
+      <Form
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+        className={css.form}>
+        <Row className='mb-4'>
+          <Form.Group as={Col} md='7' controlId='validationCustom01'>
+            <Form.Label>П.І.Б</Form.Label>
+            <Form.Control required type='text' placeholder='ПІБ' />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} md='5' controlId='validationCustom02'>
+            <Form.Label>Телефон</Form.Label>
+            <Form.Control
+              required
+              type='number'
+              placeholder='+380'
+              defaultValue='+380 '
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
+          <Form.Label>Повідомлення</Form.Label>
+          <Form.Control as='textarea' rows={3} />
+        </Form.Group>
+        <Button type='submit'>Відправити</Button>
+      </Form>
     </footer>
   );
 };
